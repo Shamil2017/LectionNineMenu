9de020332aed2f54339cfe8388435daaa2e83f57
OtsokovShA <shamil24@mail.ru>
2023-10-31 15:36:36 +0300

    Код для управление меню

--- app/src/main/java/com/example/lectionninemenu/MainActivity.kt
+++ app/src/main/java/com/example/lectionninemenu/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.lectionninemenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textViewLabel:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textViewLabel = findViewById<TextView>(R.id.textView)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val itemId:Int = item.itemId;
+        if (itemId == R.id.first)
+            textViewLabel.text = "First"
+        if (itemId == R.id.second)
+            textViewLabel.text = "Second"
+        if (itemId == R.id.third)
+            textViewLabel.text = "Third"
+
+        return super.onOptionsItemSelected(item)
     }
 }
